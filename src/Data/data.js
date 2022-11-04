8b9b1d04209d01792bb15bf9dc878d6131edc9f7
Nora Vargas <noravgs92@gmail.com>
2022-11-04 13:30:17 -0500

    added new react project

--- src/Data/data.js
+++ src/Data/data.js
@@ -3,6 +3,7 @@ import Crypto from '../assets/cryptoimg.png'
 import Mix from '../assets/miximg.png'
 import Poke from '../assets/pokeimg.png'
 import hrkn from '../assets/hkrnimg.png'
+import instant from '../assets/instantimg.png'
 
 export const data=[
     {
@@ -14,27 +15,34 @@ export const data=[
     },
     {
         id:2,
+        name:"WIP Coffee Ordering and Delivery React Application",
+        image:instant,
+        github:"https://github.com/noravgs/instant-cafe",
+        live:"https://instantcafe.netlify.app/",
+    },
+    {
+        id:3,
         name:"ReactJS Application",
         image:Crypto,
         github:"https://github.com/noravgs/crypto-app",
         live:"http://noravgs.github.io/crypto-app",
     },
     {
-        id:3,
+        id:4,
         name:"Hacker News Api, ReactJS, Taildwind Application",
         image:hrkn,
         github:"https://github.com/noravgs/mix-it-up-API",
         live:"https://noravgs.github.io/mix-it-up-API/",
     },
     {
-        id:4,
+        id:5,
         name:"Cocktail Api Application",
         image:Mix,
         github:"https://github.com/noravgs/mix-it-up-API",
         live:"https://noravgs.github.io/mix-it-up-API/",
     },
     {
-        id:5,
+        id:6,
         name:"Pokemon Api Application",
         image:Poke,
         github:"https://github.com/noravgs/poke-api",
